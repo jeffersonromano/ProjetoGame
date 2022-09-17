ee2d7362337d1a8bc222e2dc8852e348cdc64250
Jefferson Romano <jeffersonromano@gmail.com>
2022-09-17 00:53:36 +0000

    Modificação do programa para uso de funções. Implementado busca de perguntas por nível de dificuldade.

--- main.py
+++ main.py
@@ -4,29 +4,55 @@
 # Data: 16-09-2022
 # Versão: 0.1
 
-# libs
+# imports libs
+import random
 
-# Apresentação do programa ao usuário
-print(
-    '\nBem-vindo ao Quiz Rural!!!\nResponda as perguntas e ganhe pontos! O objetivo é ficar entre os primeiros no ranking geral!\nVamos nessa!'
-)
-
-
-#nomeJogador = input('Digite seu nome: ')
-# Lendo as perguntas de um arquivo externo. As perguntas e respostas estão separadas
-# pelo caractere | (pipe)
-with open("base-de-perguntas-e-respostas.txt", encoding='utf-8') as f:
-    # Criando um array com os dados de uma linha e quebrando em valores com a função split
-    listaDePerguntas = [line.split("|") for line in f]
-
-# Criar um contador para identificar a ordem da questão no dicionário "perguntas"
-count = 1
+# Lista de variáveis
+listaDePerguntas = []
+listaDePerguntasRS = []
 perguntas = {}
 
-# Incrementando o dicionário
-for x in listaDePerguntas:
+def limparPerguntas():
+  perguntas = {}
+def adicionarPergunta():
+  pergunta = input('Digite a pergunta seguida de ponto de interrogação (?):')
+  alternativa1 = input('Digite o texto da alternativa a): ')
+  alternativa2 = input('Digite o texto da alternativa b): ')
+  alternativa3 = input('Digite o texto da alternativa c): ')
+  alternativaCorreta = input('Digite a letra da alternativa correta: ')
+  nivelPergunta = input('Qual o nível da pergunta? Digite: f=fácil, m=médio e d=difícil: ')
+
+  line = pergunta + "|" + alternativa1 + "|" + alternativa2 + "|" + alternativa3 + "|" + alternativaCorreta + "|" + nivelPergunta + "\n"
+  # Abrir arquivo para inclusão
+  with open("base-de-perguntas-e-respostas.txt", 'a') as f:
+    f.write(line)
+
+  carregarPerguntas()
+
+def carregarPerguntas(nivel = 'f'):
+  
+  # Lendo as perguntas de um arquivo externo. As perguntas e respostas estão separadas
+  # pelo caractere | (pipe)
+  with open("base-de-perguntas-e-respostas.txt", encoding='utf-8') as f:
+    # Criando um array com os dados de uma linha e quebrando em valores com a função split
+    listaDePerguntas = [line.split("|") for line in f]
+    #print(listaDePerguntas)
+    
+    listaPorNivel = [x for x in listaDePerguntas if x[5].strip() == nivel]
+    
+    #print(listaPorNivel)
+    
+    # Sorteando perguntas
+    listaDePerguntasRS = random.sample(listaPorNivel, len(listaPorNivel))
+
+  # Criar um contador para identificar a sequência das questões no dicionário "perguntas"
+  count = 1
+  
+  # Alimentando o dicionário
+  for x in listaDePerguntasRS:
     # Criando a chave com numeração para cada pergunta.
-    # Usando a função strip() para remover espaços em branco antes e depois do valor, pois na comparação com a resposta do usuário (linha 55) dá falha
+    # Usando a função strip() para remover espaços em branco antes e depois do valor, 
+    #pois na comparação com a resposta do usuário (linha 55) dá falha
     perguntas['Pergunta ' + str(count)] = {
         'pergunta': x[0],
         'respostas': {
@@ -34,35 +60,84 @@ for x in listaDePerguntas:
             'b': x[2].strip(),
             'c': x[3].strip()
         },
-        'respostaCerta': x[4].strip()
+        'respostaCerta': x[4].strip(),
+        'nivel': x[5].strip()
     }
     count += 1
+  #return print(perguntas)
+  return perguntas
 
-# Variável para contabilizar os acertos
-totalAcertos = 0
+def jogar():
 
-# Utilzando um for para percorrrer as perguntas e suas respectivas respostas, onde pk se refere a chave da pergunta e pv ao valor da pergunta dentro do dicionário "perguntas"
-for pk, pv in perguntas.items():
-  # Listando a pergunta
-  print(f'\n{pk}: {pv["pergunta"]}\n')
-  print(pv['respostaCerta'])
-  # listando as respostas da pergunta
-  for rk, rv in pv['respostas'].items():
-    print(f'{rk}){rv}')
+  print('\nIniciando partida!\n')
+  nivel = input('Qual o nível de dificuldade desejado?\n(f=fácil, m=médio e d=difícil): ')
+  
+  perguntas = carregarPerguntas(nivel)
+  
+  # Apresentação do programa ao usuário
+  print(
+    '\nBem-vindo ao Quiz Rural!!!\nResponda as perguntas e ganhe pontos!\nO objetivo é ficar entre os primeiros no ranking geral!\nVamos nessa!'
+  )
 
-  # Obtendo a resposta do usuário em cada pergunta
-  respostaUsuario = input('\nDigite a letra correspondente a sua resposta: ')
+  # Identificando o jogador
+  nomeJogador = input('Digite seu nome: ')
+  
+  totalAcertos = 0
+  contador = 0
 
-  #print(type(pv['respostaCerta']))
-  #print(type(respostaUsuario))
+  # Utilzando um for para percorrrer as perguntas e suas respectivas respostas, 
+  #onde pk se refere a chave da pergunta e pv ao valor da pergunta dentro do dicionário "perguntas"
+  #print(perguntas)
+  for pk, pv in perguntas.items():
+    #print(len(perguntas))
+    #print(contador)
+    
+    # Listando a pergunta
+    print(f'\n{pk}: {pv["pergunta"]}\n')
+    print(pv['respostaCerta'])
+    # listando as respostas da pergunta
+    for rk, rv in pv['respostas'].items():
+      print(f'{rk}){rv}')
+  
+    # Obtendo a resposta do usuário em cada pergunta
+    respostaUsuario = input('\nDigite a letra correspondente a sua resposta: ')
+  
+    #print(type(pv['respostaCerta']))
+    #print(type(respostaUsuario))
+  
+    # verificando se o usuário acertou ou não e dando feedback imediato
+    if respostaUsuario.lower() == pv['respostaCerta']:
+      totalAcertos += 1
+      print("Aaaaaaacertou, miseravi!!!")
+    else:
+      print("Êeeeeeerrou!!! Que merda hein?! Sabia não :(")
+  
+    contador += 1
+    # Gravando o ranking da partida
+    if contador == len(perguntas):
+      # Percentual de acertos
+      #percentualAcertos = totalAcertos / qtdPerguntas * 100
+      #print("\nPercentual de acerto: {:.2f}%".format(percentualAcertos))
+      line = nomeJogador+'|'+str(totalAcertos)+'\n'
+      with open("ranking-jogadores.txt", 'a+') as r:
+        # r.write(nomeJogador)
+        # r.write('|')
+        # r.write(str(totalAcertos))
+        # r.write('\n')
+        r.writelines(line)
+      limparPerguntas()
+      jogar()
 
-  # verificando se o usuário acertou ou não e dando feedback imediato
-  if respostaUsuario.lower() == pv['respostaCerta']:
-    totalAcertos += 1
-    print("Aaaaaaacertou, miseravi!!!")
-  else:
-    print("Êeeeeeerrou!!! Que merda hein?! Sabia não :(")
+# Exibir menu de opções
+print('Seja bem-vindo! Qual das opções a seguir você deseja realizar?\n\n')
+print('1 - Adicionar pergunta ao banco de perguntas')
+print('2 - Remover pergunta do banco de perguntas')
+print('3 - Alterar pergunta do banco de perguntas')
+print('4 - Buscar uma pergunta no banco de perguntas')
+print('5 - Jogar')
+print('0 - Encerrar o programa')
 
-pontosDaRodada = 0
-PerguntasPorRodada = 5
-contador = 0
+opcao = int(input('Digite o número da opção desejada: '))
+
+if  opcao == 5:
+  jogar()
